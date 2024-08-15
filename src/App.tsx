d08baa4018e9deb2993f783fc37a392dbe84388d
Adithyan A <deepaadithyan56@gmail.com>
2024-08-16 03:08:14 +0530

    Refactor: Move BrowserRouter wrapper to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Navbar } from "@/components/navbar";
 import { ThemeProvider } from "@/components/providers/theme-provider.tsx";
 
@@ -7,6 +8,12 @@ function App() {
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Navbar />
         <h1> Hi There</h1>
+
+        <Routes>
+          <Route path="/signin" element={<></>} />
+          <Route path="/signup" element={<></>} />
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Routes>
       </ThemeProvider>
     </>
   );
