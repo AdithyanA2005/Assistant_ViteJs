8e67088522e70335fa64c93a816dbb655abbe8c0
Adithyan A <deepaadithyan56@gmail.com>
2024-08-16 02:21:39 +0530

    Create a theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { ThemeProvider } from "@/components/providers/theme-provider.tsx";
 function App() {
   return (
     <>
       <h1> Hi There</h1>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <h1> Hi There</h1>
+      </ThemeProvider>
     </>
   );
 }
