d361a42cb046423f5cd6006079c803065f9e6eed
Adithyan A <deepaadithyan56@gmail.com>
2024-08-16 03:32:52 +0530

    Setup environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_AW_PROJECT_ENDPOINT: string;
+  readonly VITE_AW_PROJECT_ID: string;
+  readonly VITE_AW_DATABASE_ID: string;
+  readonly VITE_AW_PROFILE_PHOTO_STORAGE_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
